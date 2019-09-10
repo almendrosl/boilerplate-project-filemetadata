e889ef0cd970bdd5ec07d45c2665f2d6767b8dfa
Lucas Matias Almendros <lucas.almendros@globant.com>
2019-09-10 16:20:47 -0300

    first configs

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-'use strict';
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable linebreak-style */
+const express = require('express');
+const cors = require('cors');
 
-var express = require('express');
-var cors = require('cors');
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  require('dotenv').config();
+}
 
 // require and use "multer"...
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.get('/hello', (req, res) => {
+  res.json({ greetings: 'Hello, API' });
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Node.js listening ...');
 });
